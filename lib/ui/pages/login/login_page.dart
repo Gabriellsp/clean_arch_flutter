087f3b9cf4ea0d443e32e197223f2f306a505c6a
gabriellsp <gabriel.luis@dtidigital.com.br>
2022-03-23 12:00:58 -0300

    create loginButton

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -59,18 +59,7 @@ class _LoginPageState extends State<LoginPage> {
                           padding: const EdgeInsets.only(top: 8.0, bottom: 32),
                           child: PasswordInput(),
                         ),
-                        StreamBuilder<bool?>(
-                            stream: widget.presenter!.isFormValidStream,
-                            builder: (context, snapshot) {
-                              return ElevatedButton(
-                                onPressed: snapshot.data == true
-                                    ? widget.presenter!.auth
-                                    : null,
-                                child: Text(
-                                  'Entrar'.toUpperCase(),
-                                ),
-                              );
-                            }),
+                        LoginButton(),
                         TextButton.icon(
                           onPressed: null,
                           icon: const Icon(Icons.person),
