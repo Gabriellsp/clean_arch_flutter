5364b0146f91c249d0fda39a26918c76b0ab5dc7
gabriellsp <gabriel.luis@dtidigital.com.br>
2022-03-24 14:53:03 -0300

    move emailValidate to prodution

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,21 +1,6 @@
 import 'package:test/test.dart';
 
-import 'package:clean_archtecture/validation/protocols/field_validation.dart';
-
-class EmailValidation implements FieldValidation {
-  final String field;
-  EmailValidation(
-    this.field,
-  );
-
-  @override
-  String? validate(String? value) {
-    final regex = RegExp(
-        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value!);
-    return isValid ? null : 'Campo inválido';
-  }
-}
+import 'package:clean_archtecture/validation/validators/email_validation.dart';
 
 void main() {
   late EmailValidation? sut;
