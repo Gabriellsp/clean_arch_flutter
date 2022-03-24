8d1eccf5e61067370bf34696831b774ecf313929
gabriellsp <gabriel.luis@dtidigital.com.br>
2022-03-24 14:32:09 -0300

    create archives prodution

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -1,33 +1,25 @@
+import 'package:clean_archtecture/validation/validators/validators.dart';
 import 'package:test/test.dart';
 
-abstract class FieldValidation {
-  String? get field;
-  String? validate(String? value);
-}
-
-class RequiredFieldValidation implements FieldValidation {
-  final String? field;
-
-  RequiredFieldValidation(this.field);
-
-  @override
-  String? validate(String? value) {
-    return value!.isEmpty ? 'Campo obrigatório' : null;
-  }
-}
-
 void main() {
-  test('Should return null if value is not empty', () {
-    final sut = RequiredFieldValidation('any_field');
-    final error = sut.validate('any_value');
+  late RequiredFieldValidation? sut;
 
+  setUp(() {
+    sut = RequiredFieldValidation('any_field');
+  });
+
+  test('Should return null if value is not empty', () {
+    final error = sut!.validate('any_value');
     expect(error, null);
   });
 
   test('Should return null if value is not empty', () {
-    final sut = RequiredFieldValidation('any_field');
-    final error = sut.validate('');
+    final error = sut!.validate('');
+    expect(error, 'Campo obrigatório');
+  });
 
+  test('Should return null if value is not empty', () {
+    final error = sut!.validate(null);
     expect(error, 'Campo obrigatório');
   });
 }
